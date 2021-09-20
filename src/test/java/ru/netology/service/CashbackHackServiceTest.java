978b6e74d7ae4ab91b32e20f6ddc2856d7c3ba1d
mslancer <wolfnw@yandex.ru>
2021-09-20 23:41:19 +0500

    junit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,54 +1,52 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
     CashbackHackService cashback = new CashbackHackService();
 
     @Test
-    public void shouldBuy800() {
-    int actual = cashback.remain(800);
-    int expected = 200;
-    assertEquals(actual,expected);
+    public void shouldBuy800(){
+    int expected = cashback.remain(800);
+    int actual = 200;
+    assertEquals(expected,actual);
     }
 
     @Test
-    public void shouldBuyZero() {
-        int actual = cashback.remain(0);
-        int expected = 1000;
-        assertEquals(actual,expected);
+    public void shouldBuy500(){
+        int expected = cashback.remain(500);
+        int actual = 500;
+        assertEquals(expected,actual);
     }
 
     @Test
-    public void shouldBuy500() {
-        int actual = cashback.remain(500);
-        int expected = 500;
-        assertEquals(actual,expected);
+    public void shouldBuy999(){
+        int expected = cashback.remain(999);
+        int actual = 1;
+        assertEquals(expected,actual);
     }
 
     @Test
-    public void shouldBuy999() {
-        int actual = cashback.remain(999);
-        int expected = 1;
-        assertEquals(actual,expected);
+    public void shouldBuy1(){
+        int expected = cashback.remain(1);
+        int actual = 999;
+        assertEquals(expected,actual);
     }
 
     @Test
-    public void shouldBuy1() {
-        int actual = cashback.remain(1);
-        int expected = 999;
-        assertEquals(actual,expected);
+    public void shouldBuy0(){
+        int expected = cashback.remain(0);
+        int actual = 1000;
+        assertEquals(expected,actual);
     }
 
     @Test
-    public void shouldBuy1000() {
-        int actual = cashback.remain(1000);
-        int expected = 1;
-        assertEquals(actual,expected);
+    public void shouldBuy1000(){
+        int expected = cashback.remain(1000);
+        int actual = 0;
+        assertEquals(expected,actual);
     }
-
-
 
 }
